aaab8c5c05d107ed056851a56196e12a6abbe6b2
Pentagon22GIT <153830524+Pentagon22GIT@users.noreply.github.com>
2025-04-08 22:33:43 +0900

    魅力の削除

--- components/charm/charm.tsx
+++ components/charm/charm.tsx
@@ -5,66 +5,7 @@ export default function Charm() {
     return (
         <>
             <div className="charm_box">
-                <a href="/map" className="charm_a_box">
-                    <div className="charm_header">
-                        <Image
-                            className="charm_img"
-                            src="/topad/mapad.png"
-                            alt=""
-                            width={2640}
-                            height={1640}
-                            priority
-                        />
-                    </div>
-                    <div className="charm_desc">
-                        <h1>MAP誘導</h1>
-                        <p>立体でより分かりやすく</p>
-                    </div>
-                </a>
-                <a href="/event" className="charm_a_box">
-                    <div className="charm_header">
-                        <Image
-                            className="charm_img"
-                            src="/topad/searchad.png"
-                            alt=""
-                            width={2360}
-                            height={1640}
-                            priority
-                        />
-                    </div>
-                    <div className="charm_desc">
-                        <h1>検索誘導</h1>
-                        <p>今までにない便利さを追求しました</p>
-                    </div>
-                </a>
-                <a href="/timetable" className="charm_a_box">
-                    <div className="charm_header">
-                        <Image
-                            className="charm_img"
-                            src="/topad/timead.png"
-                            alt=""
-                            width={2360}
-                            height={1640}
-                            priority
-                        />
-                    </div>
-                    <div className="charm_desc">
-                        <h1>タイムテーブル誘導</h1>
-                        <p>ステージで今やってるものを確認</p>
-                    </div>
-                </a>
-                <div className="charm_sp">
-                    <div className="charm_header_sp">
-                        <Image
-                            className="charm_img_sp"
-                            src="/welcome/logosp.png"
-                            alt=""
-                            width={831}
-                            height={714}
-                            priority
-                        />
-                    </div>
-                </div>
+                mapコンテンツ ※画像未定
             </div>
         </>
     );
