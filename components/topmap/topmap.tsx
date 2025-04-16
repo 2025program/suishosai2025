8b76845e515c6303651fc67d9acfe2492d780a02
Pentagon22GIT <153830524+Pentagon22GIT@users.noreply.github.com>
2025-04-16 21:21:00 +0900

    検索UIの完成

--- components/topmap/topmap.tsx
+++ components/topmap/topmap.tsx
@@ -1,55 +1,82 @@
 import "./topmap.css"
 import Image from 'next/image';
-import { Mochiy_Pop_One } from "next/font/google";
-const mochiy = Mochiy_Pop_One({ weight: "400", subsets: ["latin"] });
 
 export default function Topmap() {
     return (
         <>
             <div className="topsearch">
                 <div className="topsearch_event">
-                    <a href="/event" className="search_event">
-                        <div className="title-top">
-                            <p className={mochiy.className}>イベント検索</p>
-                        </div>
-                        <Image
-                            className="top_event_icon"
-                            src="/topsearch/search.svg"
-                            alt=""
-                            width={1000}
-                            height={1000}
-                            priority
-                        />
+                    <a href="/event" className="_search_top">
+                        <span className="search_button__inner">
+                            <span className="btn-slide-t">
+                                <span className="index-wrapper">
+                                    <span className="index index3">time</span>
+                                    <span className="index index2">map</span>
+                                    <span className="index index1">event</span>
+                                </span>
+
+                                <div className="btn-backer-blue"></div>
+                            </span>
+                            <span className="top_search_text">イベント検索</span>
+                            <Image
+                                className="top__icon"
+                                src="/topsearch/search.svg"
+                                alt=""
+                                width={1000}
+                                height={1000}
+                                priority
+                            />
+                        </span>
+                        <span className="search_effect" data-rounded="rounded-lg"></span>
                     </a>
                 </div>
 
                 <div className="bottom_search">
                     <div className="bottom_grid">
                         <a href="/map" className="_search">
-                            <div className="title-top">
-                                <p className={mochiy.className}>学校マップ</p>
-                            </div>
-                            <Image
-                                className="top_map_icon"
-                                src="/topsearch/map.svg"
-                                alt=""
-                                width={1000}
-                                height={1000}
-                                priority
-                            />
+                            <span className="search_button__inner">
+                                <span className="btn-slide">
+                                    <span className="index-wrapper">
+                                        <span className="index index3">time</span>
+                                        <span className="index index1">event</span>
+                                        <span className="index index2">map</span>
+                                    </span>
+                                    <div className="btn-backer-yellow"></div>
+                                </span>
+                                <span className="top_search_text_b">校内マップ</span>
+                                <Image
+                                    className="top__icon"
+                                    src="/topsearch/map.svg"
+                                    alt=""
+                                    width={1000}
+                                    height={1000}
+                                    priority
+                                />
+                            </span>
+                            <span className="search_effect" data-rounded="rounded-lg"></span>
                         </a>
                         <a href="/timetable" className="_search">
-                            <div className="title-top">
-                                <p className={mochiy.className}>タイムテーブル</p>
-                            </div>
-                            <Image
-                                className="top_time_icon"
-                                src="/topsearch/calendar.svg"
-                                alt=""
-                                width={1000}
-                                height={1000}
-                                priority
-                            />
+                            <span className="search_button__inner">
+                                <span className="btn-slide">
+                                    <span className="index-wrapper">
+                                        <span className="index index1">event</span>
+                                        <span className="index index2">map</span>
+                                        <span className="index index3">time</span>
+                                    </span>
+
+                                    <div className="btn-backer-red"></div>
+                                </span>
+                                <span className="top_search_text_b">タイムテーブル</span>
+                                <Image
+                                    className="top__icon"
+                                    src="/topsearch/calendar.svg"
+                                    alt=""
+                                    width={1000}
+                                    height={1000}
+                                    priority
+                                />
+                            </span>
+                            <span className="search_effect" data-rounded="rounded-lg"></span>
                         </a>
                     </div>
                 </div>
