672ccc6e5a60cb86aab75e8ba1acac99d66122b5
Pentagon22GIT <153830524+Pentagon22GIT@users.noreply.github.com>
2025-04-25 23:36:16 +0900

    topページを編集しやすく

--- app/page.tsx
+++ app/page.tsx
@@ -13,139 +13,139 @@ export default function Home() {
   return (
     <>
       <div className="body">
-        <main>
-          <div className="hidden-x-side">
+        <div className="hidden-x-side">
 
-            {/** ヘッダー */}
-            <section className="headtop">
-              <div className="headtop-leftarea">
-                <div className="headtop-left">
+          {/** ヘッダー */}
+          <section className="headtop">
+            <div className="headtop-leftarea">
+              <div className="headtop-left">
 
-                  <div className="headtop-themelogo">
-                    <picture>
-                      <source
-                        media="(min-width:768px)"
-                        srcSet="/welcome/logopc.png"
-                      />
-                      <Image
-                        className="headtop-sp img"
-                        src="/welcome/logosp.png"
-                        alt=""
-                        width={900}
-                        height={850}
-                        priority
-                      />
-                    </picture>
-                  </div>
-
-                  <div className="headtop-change">
-                    <picture>
-                      <source
-                        media="(min-width:768px)"
-                        srcSet="/welcome/themepc.png"
-                      />
-                      <Image
-                        className="headtop-sp img"
-                        src="/welcome/themesp.png"
-                        alt="neo"
-                        width={180}
-                        height={1164}
-                        priority
-                      />
-                    </picture>
-                  </div>
-
+                <div className="headtop-themelogo">
+                  <picture>
+                    <source
+                      media="(min-width:768px)"
+                      srcSet="/welcome/logopc.png"
+                    />
+                    <Image
+                      className="headtop-sp img"
+                      src="/welcome/logosp.png"
+                      alt=""
+                      width={900}
+                      height={850}
+                      priority
+                    />
+                  </picture>
                 </div>
-              </div>
 
-              <div className="headtop-right">
-                <picture>
-                  <source
-                    media="(min-width:1024px)"
-                    srcSet="/welcome/rightpc.png"
-                  />
-                  <source
-                    media="(min-width:660px)"
-                    srcSet="/welcome/rightpd.png"
-                  />
-                  <Image
-                    className="headtop-right-sp img"
-                    src="/welcome/rightsp.png"
-                    alt=""
-                    width={3000}
-                    height={2000}
-                    priority
-                  />
-                </picture>
-              </div>
-
-              <a href="#_" className='payment-btn'>お支払いについて</a>
-
-            </section>
-
-
-            <DevOnly>
-              <>
-                {/** マップ */}
-                <section className='headbox mapbox-up'>
-                  <Image
-                    className="top_index"
-                    src="/welcome/index_search.png"
-                    alt=""
-                    width={800}
-                    height={300}
-                    priority
-                  />
-                  <TopSelect />
-                </section>
-
-                {/** フォーム */}
-                <section className='headbox formbox-up '>
-                  <Image
-                    className="top_index"
-                    src="/welcome/index_form.png"
-                    alt=""
-                    width={800}
-                    height={300}
-                    priority
-                  />
-                  <Form />
-                </section>
-
-                {/** ニュース */}
-                <section className="headbox newsbox-up">
-                  <Image
-                    className="top_index"
-                    src="/welcome/index_news.png"
-                    alt=""
-                    width={800}
-                    height={300}
-                    priority
-                  />
-                  <Announce />
-                </section>
-
-                {/** アクセス */}
-                <section className='headbox accessbox-up'>
-                  <Image
-                    className="top_index"
-                    src="/welcome/index_access.png"
-                    alt=""
-                    width={800}
-                    height={300}
-                    priority
-                  />
-                  <Access />
-                </section>
-
-                {/** フッターコンテンツ */}
-                <div className="headbox">
-                  <Footer />
+                <div className="headtop-change">
+                  <picture>
+                    <source
+                      media="(min-width:768px)"
+                      srcSet="/welcome/themepc.png"
+                    />
+                    <Image
+                      className="headtop-sp img"
+                      src="/welcome/themesp.png"
+                      alt="neo"
+                      width={180}
+                      height={1164}
+                      priority
+                    />
+                  </picture>
                 </div>
-              </>
-            </DevOnly>
-          </div>
-        </main>
+
+              </div>
+            </div>
+
+            <div className="headtop-right">
+              <picture>
+                <source
+                  media="(min-width:1024px)"
+                  srcSet="/welcome/rightpc.png"
+                />
+                <source
+                  media="(min-width:660px)"
+                  srcSet="/welcome/rightpd.png"
+                />
+                <Image
+                  className="headtop-right-sp img"
+                  src="/welcome/rightsp.png"
+                  alt=""
+                  width={3000}
+                  height={2000}
+                  priority
+                />
+              </picture>
+            </div>
+
+            <a href="#_" className='payment-btn'>お支払いについて</a>
+
+          </section>
+
+
+          <DevOnly>
+            <>
+              {/** マップ */}
+              <section className='headbox mapbox-up'>
+                <Image
+                  className="top_index"
+                  src="/welcome/index_search.png"
+                  alt=""
+                  width={800}
+                  height={300}
+                  priority
+                />
+                {/** <TopSelect /> */}
+                <div className="test-content"></div>
+              </section>
+
+              {/** フォーム */}
+              <section className='headbox formbox-up '>
+                <Image
+                  className="top_index"
+                  src="/welcome/index_form.png"
+                  alt=""
+                  width={800}
+                  height={300}
+                  priority
+                />
+                {/** <Form /> */}
+                <div className="test-content"></div>
+              </section>
+
+              {/** ニュース */}
+              <section className="headbox newsbox-up">
+                <Image
+                  className="top_index"
+                  src="/welcome/index_news.png"
+                  alt=""
+                  width={800}
+                  height={300}
+                  priority
+                />
+                {/** <Announce /> */}
+                <div className="test-content"></div>
+              </section>
+
+              {/** アクセス */}
+              <section className='headbox accessbox-up'>
+                <Image
+                  className="top_index"
+                  src="/welcome/index_access.png"
+                  alt=""
+                  width={800}
+                  height={300}
+                  priority
+                />
+                {/** <Access /> */}
+                <div className="test-content"></div>
+              </section>
+
+              {/** フッターコンテンツ */}
+              {/** <Footer /> */}
+            </>
+          </DevOnly>
+        </div>
       </div>
     </>
   );
