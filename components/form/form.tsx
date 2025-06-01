34db06fe28bf8d8290044829916328f2f7a47a44
2025program <suiranprogram@gmail.com>
2025-06-01 21:50:32 +0900

    カラーリング調整

--- components/form/form.tsx
+++ components/form/form.tsx
@@ -65,6 +65,29 @@ export default function Form() {
                         <p>校内にあるパンフレットからも投票可能です。</p>
                         <p>1人1回、各部門1団体投票してください。ただし、自分が所属する団体には投票できません。</p>
                     </div>
+
+                    <div className="form_back">
+                        <Image
+                            className="form_topper"
+                            src="/about/form.png"
+                            alt=""
+                            width={2000}
+                            height={1000}
+                            priority
+                        />
+                    </div>
+                    <div className="anc">
+                        <p>翠翔祭に関するアンケートです。次年度以降のためにご回答をよろしくお願いします。</p>
+                        <p>※在校生は回答しないでください</p>
+                        <a href="https://docs.google.com/forms/d/1scpgu1SGEyP_c6XRBVb_u_5e_IZ1V1DWPFbt1OsJO1I/edit?usp=drivesdk" className="relative inline-block text-lg group">
+                            <span className="relative z-10 block px-5 py-2 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                                <span className="absolute inset-0 w-full h-full px-5 py-2 rounded-lg bg-gray-50"></span>
+                                <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                                <span className="relative">アンケート</span>
+                            </span>
+                            <span className="absolute bottom-0 right-0 w-full h-10 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
